c9f5f6d0b047057dbda42901741152b18238de4b
Alex <alex@tinkin.one>
2021-05-08 18:00:56 -0500

    change database config

--- config/database.js
+++ config/database.js
@@ -4,12 +4,10 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        uri:  env('DATABASE_URI'),
         srv: env.bool('DATABASE_SRV', false),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'backend'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+        database: env('DATABASE_NAME', 'smartrisk'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
