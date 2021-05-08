5d0d835a579660443fa45dec7c11d695574d00ac
Alex <alex@tinkin.one>
2021-05-08 12:41:38 -0500

    change dump

--- fixtures/restore.sh
+++ fixtures/restore.sh
@@ -6,7 +6,7 @@ URI=${1:-${DATABASE_URI:-mongodb://localhost:27017}}
 
 DUMP=$( cd ./fixtures/dump/smartrisk; pwd -P )
 echo -e "Loading fixtures in database \n\n"
-echo $DUMP
+echo $URI
 mongorestore $DUMP --uri $URI --db ${2:-smartrisk} --drop
 
 echo -e "\n\n FINISHED LOADING FIXTURES!"
